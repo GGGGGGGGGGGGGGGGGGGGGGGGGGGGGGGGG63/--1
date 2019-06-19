30fbe189baed601d8603e4298b96a001bfc081bd
GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63 <51705641+GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63@users.noreply.github.com>
2019-06-19 20:49:31 +0800

    2
    .

--- 歌手/main.cpp
+++ 歌手/main.cpp
@@ -10,9 +10,13 @@
 #include <fstream>
 using namespace std;
 int main() {
+    int a,b;
     ofstream gg("/Users/s20181105880/Desktop/歌手.txt");
+    ifstream GG("/Users/s20181105880/Desktop/歌手的副本.txt ");
+    GG >> a>>b;
     if(gg.is_open())
     {
+        gg<<"c="<<a+b<<endl;
         gg<<"This is a line.\n";
         gg <<"This is another line.\n";
         gg.close();
