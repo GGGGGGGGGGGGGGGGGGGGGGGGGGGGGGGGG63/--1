8380defc09657af6b1313746de825eebe2f80499
GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63 <51705641+GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63@users.noreply.github.com>
2019-06-19 21:28:15 +0800

    4
    4

--- 歌手/main.cpp
+++ 歌手/main.cpp
@@ -12,7 +12,7 @@ using namespace std;
 int main() {
     int a,b;
     ofstream gg("/Users/s20181105880/Desktop/歌手.txt");
-    ifstream GG("/Users/s20181105880/Desktop/歌手的副本.txt ");
+    ifstream GG("/Users/s20181105880/Desktop/歌手的副本.txt");
     if(GG.is_open())
     {
         cout<<"file is ok"<<endl;
