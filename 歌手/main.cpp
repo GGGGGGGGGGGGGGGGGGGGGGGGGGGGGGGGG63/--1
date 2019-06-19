31002e82c2303a49d64bc82e45e795532a1d4c16
GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63 <51705641+GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63@users.noreply.github.com>
2019-06-19 19:57:45 +0800

    Initial Commit

---
+++ 歌手/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  歌手
+//
+//  Created by s20181105880 on 2019/6/19.
+//  Copyright © 2019 。. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
