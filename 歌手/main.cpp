6ea9121fa111155adaa2648ffda3f9213ea25608
GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63 <51705641+GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63@users.noreply.github.com>
2019-06-26 21:32:26 +0800

    11
    11

--- 歌手/main.cpp
+++ 歌手/main.cpp
@@ -25,23 +25,45 @@ struct Student{
 }student[6];
 
 int main()
-{
-    int i=0;
-    ifstream Refereein("E:\裁判.txt");
-    ifstream Studentin("E:\参赛者.txt");
-    ofstream allout("E:\输出.xls");
+{   int i;
+    ifstream Refereein("/Users/s20181105880/Desktop/裁判.txt");
+    ifstream Studentin("/Users/s20181105880/Desktop/参赛者.txt");
+    ofstream allout("/Users/s20181105880/Desktop/输出.xls");
     if(Refereein.is_open())//裁判输入
     {
         while(!Refereein.eof() && i<7)
         {
-            Refereein>>referee[i].name>>referee[i].grade[0]>>referee[i].grade[1]>>referee[i].grade[2]>>referee[i].grade[3]>>referee[i].grade[4]>>referee[i].grade[5];
+        Refereein>>referee[i].name>>referee[i].grade[0]>>referee[i].grade[1]>>referee[i].grade[2]>>referee[i].grade[3]>>referee[i].grade[4]>>referee[i].grade[5];
             i++;
         }
         Refereein.close();
     }
+    i=0;
     if (Studentin.is_open())//参赛者输入
     {
-       
+        while(!Studentin.eof() && i<6)
+        {
+         Studentin>>student[i].Number>>student[i].name>>student[i].sex>>student[i].college;
+             i++;
+        }
         Studentin.close();
-    }     return 0;
+    }
+    for(int i=0;i<6;i++)
+    {
+        for(int j=0;j<7;j++)
+        {
+            student[i].grades[j]=referee[j].grade[i];
+        }
+        
+        sort(student[i].grades,student[i].grades+7);
+        
+        for(int k=1;k<6;k++)
+        {
+            student[i].sum+=student[i].grades[k];
+        }
+        student[i].sum=student[i].sum/5;
+    }
+
+    
+    return 0;
 }
