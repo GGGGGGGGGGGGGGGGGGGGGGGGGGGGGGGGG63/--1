9bcc04aea1efeb9113eb90796de4e2c01535b84c
GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63 <51705641+GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63@users.noreply.github.com>
2019-07-03 19:55:30 +0800

    宏定义
    宏定义

--- 歌手/main.cpp
+++ 歌手/main.cpp
@@ -4,7 +4,8 @@
 //
 //  Created by s20181105880 on 2019/6/19.
 //  Copyright © 2019 。. All rights reserved.
-//
+#define STUDENTNUMBER 6
+#define REFEREENUMBER 7
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
@@ -13,52 +14,52 @@
 using namespace std;
 struct Referee{
     string name;
-    double grade[7];
-} referee[7];
+    double grade[STUDENTNUMBER];
+}referee[REFEREENUMBER];
 struct Student{
     string Number;
     string name;
     string college;
     string sex;
-    double grades[7];
+    double grades[REFEREENUMBER];
     double sum=0;
-}student[6];
+}student[STUDENTNUMBER];
 bool judge(Student &object1,Student &object2)
 {
     if(object1.sum>object2.sum)
-    {
-        
         return true;
-    }
     else
-    {
         return false;
-    }
 }
 int main()
 {   int i=0;
-    ifstream Refereein("/Users/s20181105880/Desktop/裁判.txt"  );
-    ifstream Studentin("/Users/s20181105880/Desktop/参赛者.txt" );
-    ofstream allout   ("/Users/s20181105880/Desktop/输出.xls"  );
+    ifstream Refereein("/Users/s20181105880/Desktop/裁判.txt");
+    ifstream Studentin("/Users/s20181105880/Desktop/参赛者.txt");
+    ofstream allout   ("/Users/s20181105880/Desktop/输出.xls");
     if(Refereein.is_open())//裁判输入
     {
-        while(!Refereein.eof() && i<7)
+        while(!Refereein.eof() && i<REFEREENUMBER)
         {
             Refereein>>referee[i].name;
-            Refereein>>referee[i].grade[0];
+            for(int k=0;k<STUDENTNUMBER;k++)
+            {
+                Refereein>>referee[i].grade[k];
+            }
+            i++;
+            /*Refereein>>referee[i].grade[0];
             Refereein>>referee[i].grade[1];
             Refereein>>referee[i].grade[2];
             Refereein>>referee[i].grade[3];
             Refereein>>referee[i].grade[4];
             Refereein>>referee[i].grade[5];
-            i++;
+            i++;*/
         }
         Refereein.close();
     }
     i=0;
     if (Studentin.is_open())//参赛者输入
     {
-        while(!Studentin.eof() && i<6)
+        while(!Studentin.eof() && i<STUDENTNUMBER)
         {
             Studentin>>student[i].Number;
             Studentin>>student[i].name;
@@ -68,33 +69,29 @@ int main()
         }
         Studentin.close();
     }
-    for(int i=0;i<6;i++)
+    for(int i=0;i<STUDENTNUMBER;i++)
     {
-        for(int j=0;j<7;j++)
-        {
+        for(int j=0;j<REFEREENUMBER;j++)
             student[i].grades[j]=referee[j].grade[i];
-        }
-        for(int k=0;k<6;k++)
-         {
-         double min=student[i].grades[0];
-         if(student[i].grades[k]>student[i].grades[k+1])
-         {
-         min=student[i].grades[k+1];
-         student[i].grades[k+1]=student[i].grades[k];
-         student[i].grades[k]=min;
-         }
-         }
-        //sort(student[i].grades,student[i].grades+7);
-        
-        for(int k=1;k<6;k++)
+        double midnumber=0;
+        for(int k=0;k<REFEREENUMBER-1;k++)
         {
-            student[i].sum+=student[i].grades[k];
+            for(int j=0;j<REFEREENUMBER-1;j++)
+            {
+                if(student[i].grades[j]>student[i].grades[j+1])
+                {
+                    midnumber=student[i].grades[j];
+                    student[i].grades[j]=student[i].grades[j+1];
+                    student[i].grades[j+1]=midnumber;
+                }
+            }
         }
-        student[i].sum=student[i].sum/5;
+        for(int k=1;k<STUDENTNUMBER;k++)
+            student[i].sum+=student[i].grades[k];
+            student[i].sum=student[i].sum/(REFEREENUMBER-2);
     }
-    sort(student,student+6,judge);
-    
-    for(int i=0;i<6;i++)
+    sort(student,student+STUDENTNUMBER,judge);
+    for(int i=0;i<STUDENTNUMBER;i++)
     {
         allout<<student[i].Number;
         allout<<" "<<student[i].name;
@@ -103,8 +100,7 @@ int main()
         allout<<" "<<student[i].sum;
         allout<<endl;
     }
-    
-    for(int i=0;i<6;i++)
+    for(int i=0;i<STUDENTNUMBER;i++)
     {
         cout<<student[i].Number;
         cout<<" "<<student[i].name;
@@ -113,6 +109,5 @@ int main()
         cout<<" "<<student[i].sum;
         cout<<endl;
     }
-    
     return 0;
 }
