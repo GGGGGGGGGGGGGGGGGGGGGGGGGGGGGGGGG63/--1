a9f745b32c190b3cf776a56612475c923c18f193
GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63 <51705641+GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63@users.noreply.github.com>
2019-10-08 16:53:03 +0800

    数据结构
    1

--- 歌手/main.cpp
+++ 歌手/main.cpp
@@ -1,108 +1,36 @@
-//
-//  main.cpp
-//  歌手
-//
-//  Created by s20181105880 on 2019/6/19.
-//  Copyright © 2019 。. All rights reserved.
-#define STUDENTNUMBER 6
-#define REFEREENUMBER 7
-#include <stdio.h>
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <algorithm>
-using namespace std;
-struct Referee{
-    string Name;
-    double Grade[STUDENTNUMBER];
-}referee[REFEREENUMBER];
-struct Student{
-    string Number;
-    string Name;
-    string College;
-    string Sex;
-    double Grades[REFEREENUMBER];
-    double Sum=0;
-}student[STUDENTNUMBER];
-bool judge(Student &Object1,Student &Object2)
+bool Push( ElementType X, Deque D )
 {
-    if(Object1.Sum>Object2.Sum)
-        return true;
-    else
+    if((D->Rear+1)%(D->MaxSize)==D->Front)
         return false;
+    D->Front--;
+    D->Front=(D->MaxSize+D->Front)%(D->MaxSize);
+    D->Data[D->Front]=X;
+    return true;
 }
-int main()
-{   int i=0;
-    double Midnumber=0;
-    ifstream Refereein("/Users/s20181105880/Desktop/Refereein.txt");
-    ifstream Studentin("/Users/s20181105880/Desktop/Studentin.txt");
-    ofstream Outputgrade("/Users/s20181105880/Desktop/输出.xls");
-    if(Refereein.is_open())//裁判输入
-    {
-        while(!Refereein.eof() && i<REFEREENUMBER)
-        {
-            Refereein>>referee[i].Name;
-            for(int k=0;k<STUDENTNUMBER;k++)
-            {
-                Refereein>>referee[i].Grade[k];
-            }
-            i++;
-        }
-        Refereein.close();
-    }
-    i=0;
-    if (Studentin.is_open())//参赛者输入
-    {
-        while(!Studentin.eof() && i<STUDENTNUMBER)
-        {
-            Studentin>>student[i].Number;
-            Studentin>>student[i].Name;
-            Studentin>>student[i].Sex;
-            Studentin>>student[i].College;
-            i++;
-        }
-        Studentin.close();
-    }
-    for(int i=0;i<STUDENTNUMBER;i++)
-    {
-        for(int j=0;j<REFEREENUMBER;j++)
-            student[i].Grades[j]=referee[j].Grade[i];
-        for(int k=0;k<REFEREENUMBER-1;k++)
-        {
-            for(int j=0;j<REFEREENUMBER-1;j++)
-            {
-                if(student[i].Grades[j]>student[i].Grades[j+1])
-                {
-                    Midnumber=student[i].Grades[j];
-                    student[i].Grades[j]=student[i].Grades[j+1];
-                    student[i].Grades[j+1]=Midnumber;
-                }
-            }
-        }
-        for(int k=1;k<STUDENTNUMBER;k++)
-            student[i].Sum+=student[i].Grades[k];
-            student[i].Sum=student[i].Sum/(REFEREENUMBER-2);
-    }
-    sort(student,student+STUDENTNUMBER,judge);
-    for(int i=0;i<STUDENTNUMBER;i++)
-    {
-        Outputgrade<<i+1<<" ";
-        Outputgrade<<student[i].Number;
-        Outputgrade<<" "<<student[i].Name;
-        Outputgrade<<" "<<student[i].Sex;
-        Outputgrade<<" "<<student[i].College;
-        Outputgrade<<" "<<student[i].Sum;
-        Outputgrade<<endl;
-    }
-    for(int i=0;i<STUDENTNUMBER;i++)
-    {
-        cout<<i+1<<" ";
-        cout<<student[i].Number;
-        cout<<" "<<student[i].Name;
-        cout<<" "<<student[i].Sex;
-        cout<<" "<<student[i].College;
-        cout<<" "<<student[i].Sum;
-        cout<<endl;
-    }
-    return 0;
+ElementType Pop( Deque D )
+{
+    if(D->Front==D->Rear)
+        return ERROR;
+    ElementType d=D->Data[D->Front];
+    D->Front++;
+    D->Front=D->Front%(D->MaxSize);
+    return d;
+}
+bool Inject( ElementType X, Deque D )
+{
+    if((D->Rear+1)%(D->MaxSize)==D->Front)
+        return false;
+    D->Data[D->Rear]=X;
+    D->Rear=(D->Rear+1)%D->MaxSize;
+    return true;
+}
+ElementType Eject( Deque D )
+{
+    if(D->Front==D->Rear)
+        return ERROR;
+    if(!D->Rear)
+        D->Rear=D->MaxSize;
+    D->Rear--;
+    ElementType d=D->Data[D->Rear];
+    return d;
 }
