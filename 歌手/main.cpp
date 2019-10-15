c7a6413f25103dd3d47dd6ac156e3a2e603bbcee
GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63 <51705641+GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63@users.noreply.github.com>
2019-10-15 16:37:59 +0800

    指针
    指针1

--- 歌手/main.cpp
+++ 歌手/main.cpp
@@ -1,36 +1,15 @@
-bool Push( ElementType X, Deque D )
+#include <stdio.h>
+#include "stdlib.h"
+#include "string.h"
+int main()
 {
-    if((D->Rear+1)%(D->MaxSize)==D->Front)
-        return false;
-    D->Front--;
-    D->Front=(D->MaxSize+D->Front)%(D->MaxSize);
-    D->Data[D->Front]=X;
-    return true;
-}
-ElementType Pop( Deque D )
-{
-    if(D->Front==D->Rear)
-        return ERROR;
-    ElementType d=D->Data[D->Front];
-    D->Front++;
-    D->Front=D->Front%(D->MaxSize);
-    return d;
-}
-bool Inject( ElementType X, Deque D )
-{
-    if((D->Rear+1)%(D->MaxSize)==D->Front)
-        return false;
-    D->Data[D->Rear]=X;
-    D->Rear=(D->Rear+1)%D->MaxSize;
-    return true;
-}
-ElementType Eject( Deque D )
-{
-    if(D->Front==D->Rear)
-        return ERROR;
-    if(!D->Rear)
-        D->Rear=D->MaxSize;
-    D->Rear--;
-    ElementType d=D->Data[D->Rear];
-    return d;
+    char *p;
+    char s[8]="hello! ";
+    int *q;
+    int a[2]={1819043176,2105711};
+    p=(char *)a;
+    q=(int *)s;
+    printf("%d    %d    ",*q,*(q+1));
+    printf("%s\n",p);
+    return 0;
 }
