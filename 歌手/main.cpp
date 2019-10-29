d0b440d9d65d494137b7595633261c818364fc84
GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63 <51705641+GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63@users.noreply.github.com>
2019-10-29 17:39:23 +0800

    123
    1234454

--- 歌手/main.cpp
+++ 歌手/main.cpp
@@ -1,74 +1,40 @@
 
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef char ElementType;
-typedef struct TNode *Position;
-typedef Position BinTree;
-struct TNode{
-    ElementType Data;
-    BinTree Left;
-    BinTree Right;
+typedef enum {false, true} bool;
+#define MaxVertexNum 10  /* 最大顶点数设为10 */
+#define INFINITY 65535   /* ∞设为双字节无符号整数的最大值65535*/
+typedef int Vertex;      /* 用顶点下标表示顶点,为整型 */
+typedef int WeightType;  /* 边的权值设为整型 */
+
+typedef struct GNode *PtrToGNode;
+struct GNode{
+    int Nv;  /* 顶点数 */
+    int Ne;  /* 边数   */
+    WeightType G[MaxVertexNum][MaxVertexNum]; /* 邻接矩阵 */
 };
+typedef PtrToGNode MGraph; /* 以邻接矩阵存储的图类型 */
+bool Visited[MaxVertexNum]; /* 顶点的访问标记 */
 
-BinTree CreatBinTree(); /* 实现细节忽略 */
-void InorderTraversal( BinTree BT );
-void PreorderTraversal( BinTree BT );
-void PostorderTraversal( BinTree BT );
-void LevelorderTraversal( BinTree BT );
+MGraph CreateGraph(); /* 创建图并且将Visited初始化为false；裁判实现，细节不表 */
+
+void Visit( Vertex V )
+{
+    printf(" %d", V);
+}
+
+void DFS( MGraph Graph, Vertex V, void (*Visit)(Vertex) );
+
 
 int main()
 {
-    BinTree BT = CreatBinTree();
-    printf("Inorder:");    InorderTraversal(BT);    printf("\n");
-    printf("Preorder:");   PreorderTraversal(BT);   printf("\n");
-    printf("Postorder:");  PostorderTraversal(BT);  printf("\n");
-    printf("Levelorder:"); LevelorderTraversal(BT); printf("\n");
-    return 0;
-}
-void InorderTraversal( BinTree BT )
-{
-    if(BT)
-    {
-        InorderTraversal(BT->Left );
-        printf(" %c",BT->Data);
-        InorderTraversal(BT->Right);
-    }
-}
-void PreorderTraversal( BinTree BT )
-{
-    if(BT)
-    {
-        printf(" %c",BT->Data);
-        PreorderTraversal( BT->Left );
-        PreorderTraversal( BT->Right );
-        
-    }
-}
-void PostorderTraversal( BinTree BT )
-{
-    if(BT)
-    {
-        PostorderTraversal( BT->Left );
-        PostorderTraversal( BT->Right );
-        printf(" %c",BT->Data);
-    }
-}
-
-void LevelorderTraversal( BinTree BT ){
-    BinTree que[100];
-    BinTree t;
-    int head=0,rear=0;
-    if(BT)
-    {
-        que[rear++]=BT;
-        while(head!=rear)
-        {
-            t=que[head++];
-            printf(" %c",t->Data);
-            if(t->Left)que[rear++]=t->Left;
-            if(t->Right)que[rear++]=t->Right;
-        }
-    }
+    MGraph G;
+    Vertex V;
     
+    G = CreateGraph();
+    scanf("%d", &V);
+    printf("DFS from %d:", V);
+    DFS(G, V, Visit);
+    
+    return 0;
 }
